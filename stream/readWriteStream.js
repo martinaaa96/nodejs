7867b5214edb961f25ecff2657937c7f00eaae42
Martina Vangelova <mvangelovaa@abv.bg>
2023-07-09 20:57:01 +0300

    piping - stream

---
+++ stream/readWriteStream.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const readStream = fs.createReadStream('./data.txt');
+
+const writeStream = fs.createWriteStream('./datacopy.txt');
+
+readStream.on('data', (chunk)=>{
+
+    writeStream.write(chunk);
+
+});
+
+readStream.on('close', ()=>{
+    writeStream.end();
+    
+})
